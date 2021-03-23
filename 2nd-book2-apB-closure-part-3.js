16d2ac273d09fbf9b75864d38fc6dfc78e3fc58c
kwhsiung <kaiwenhsiung@gmail.com>
2021-03-23 20:42:23 +0800

    feat(2nd-book2-apB-closure-part-3): paste the suggest answer from ydkjs, and refine it

--- 2nd-book2-apB-closure-part-3.js
+++ 2nd-book2-apB-closure-part-3.js
@@ -1,13 +1,147 @@
 // https://github.com/getify/You-Dont-Know-JS/blob/2nd-ed/scope-closures/apB.md#closure-part-3
 
-function calculator() {
-  // ..
-  var computes = []
-  return function (value) {
-    if (value !== '=') {
-      computes.push(value)
-      return value
+function formatTotal(display) {
+  if (Number.isFinite(display)) {
+    // constrain display to max 11 chars
+    let maxDigits = 11;
+    // reserve space for "e+" notation?
+    if (Math.abs(display) > 99999999999) {
+      maxDigits -= 6;
     }
+    // reserve space for "-"?
+    if (display < 0) {
+      maxDigits--;
+    }
+
+    // whole number?
+    if (Number.isInteger(display)) {
+      display = display
+        .toPrecision(maxDigits)
+        .replace(/\.0+$/, "");
+    }
+    // decimal
+    else {
+      // reserve space for "."
+      maxDigits--;
+      // reserve space for leading "0"?
+      if (
+        Math.abs(display) >= 0 &&
+        Math.abs(display) < 1
+      ) {
+        maxDigits--;
+      }
+      display = display
+        .toPrecision(maxDigits)
+        .replace(/0+$/, "");
+    }
+  } else {
+    display = "ERR";
+  }
+  return display;
+}
+
+// 計算機有什麼功能，我們可以利用實體的計算機來想像
+// 數字鍵：使用者可以透過按下數字鍵，看到所按下的數字串接至一個螢幕顯示的數字區上（而由於我們並沒有顯示的需求，可直接用變數代替）
+// 運算符號鍵（加減乘除鍵）:
+//   1. 當「不是處於運算中狀態時」，使用者按下運算符號會
+//    1.1. 看到數字串列上的數字消失
+//    1.2. 儲存該數字於內部
+//    1.3. 計算機內部會知道目前正要做什麼運算（儲存目前的運算符號）
+//   2. 當「處於運算狀態時」，使用者按下運算符號會
+//    2.1. 看到數字串列上的數字消失
+//    2.2. 等同於按下「等號」
+//    2.3. 計算機內部會知道目前正要做什麼運算（儲存目前的運算符號）
+// 等號鍵：使用者按下等號鍵時，會將上個輸入的數字串列與上上個輸入的數字串列，以記憶好的運算符號，做互相運算。
+function calculator() {
+  var currentTotal = 0;
+  var currentVal = "";
+  var currentOper = "=";
+
+  return pressKey;
+
+  // ********************
+
+  function pressKey(key) {
+    // number key?
+    if (isTypingNumber(key)) {
+      appendCurrentKey(key)
+      return key;
+    }
+    // operator key?
+    else if (isTypingOperator(key)) {
+      // multiple operations in a series?
+      if (
+        isDoingOperation() &&
+        isNumberTyped()
+      ) {
+        // implied '=' keypress
+        pressKey("=");
+      } else if (isNumberTyped()) {
+        currentTotal = Number(currentVal);
+      }
+      setCurrentOper(key)
+      clearCurrentVal()
+      return key;
+    }
+    // = key?
+    else if (
+      isTypingCompute(key) &&
+      isDoingOperation()
+    ) {
+      currentTotal = op(
+        currentTotal,
+        currentOper,
+        Number(currentVal)
+      );
+      setCurrentOper('=')
+      clearCurrentVal()
+      return formatTotal(currentTotal);
+    }
+    return "";
+  };
+
+  function isTypingNumber(key) {
+    return /\d/.test(key)
+  }
+
+  function isTypingOperator(key) {
+    return  /[+*/-]/.test(key)
+  }
+
+  function isTypingCompute(key) {
+    return key == '='
+  }
+
+  function isDoingOperation() {
+    return currentOper != '='
+  }
+
+  function isNumberTyped() {
+    return currentVal != ''
+  }
+
+  function appendCurrentKey(keu) {
+    currentVal += key
+  }
+
+  function setCurrentOper(val) {
+    currentOper = val
+  }
+
+  function clearCurrentVal() {
+    currentVal = ''
+  }
+
+  function op(val1, oper, val2) {
+    var ops = {
+      // NOTE: using arrow functions
+      // only for brevity in the book
+      "+": (v1, v2) => v1 + v2,
+      "-": (v1, v2) => v1 - v2,
+      "*": (v1, v2) => v1 * v2,
+      "/": (v1, v2) => v1 / v2
+    };
+    return ops[oper](val1, val2);
   }
 }
 
